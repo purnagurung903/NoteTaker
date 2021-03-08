75af6d28beac5456442acc32ed4c94ad98025e4c
Purna Gurung <purnagurung903@gmail.com>
2021-03-07 21:29:21 -0500

    fixed code post request & delete

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,8 @@
 //require fs to write file
+const { v4: uuidv4 } = require('uuid');
+
 const fs = require("fs");
-//const path = require("path")
+const path = require("path")
 
 
 
@@ -25,8 +27,13 @@ module.exports = app => {
     fs.readFile("./db/db.json", "utf8",(err, data)=>{
       if (err)throw err;
       var note = JSON.parse(data)
-      
-    var myNote = req.body;
+      // turn this to an object
+    var myNote = {
+      title: req.body.title,
+      text: req.body.text,
+      id: uuidv4() // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+       
+      }
     
      note.push(myNote);
      //fs.writeFileSync here to write the new array with the pushed note
@@ -47,13 +54,16 @@ module.exports = app => {
       let deleteId = req.params.id;
       console.log(deleteId);
 
-      for (let i = 0; i < note.length; i++){
-        if (deleteId !== note[i].id){
-          note.splice(i, 1);
-        }
-      }
+      // for (let i = 0; i < note.length; i++){
+      //   if (deleteId !== note[i].id){
+      //     note.splice(i, 1);
+      //   }
+      // }
+      const filteredNotes = note.filter(notes => {
+        return notes.id !== deleteId
+      })
 
-      fs.writeFileSync("./db/db.json", JSON.stringify(note), "utf-8");
+      fs.writeFileSync("./db/db.json", JSON.stringify(filteredNotes), "utf-8");
      
      return res.sendStatus(200);
 
